97ae43b5aa7a127c1e3b925504c3d76a9a147178
lucas tiago <lucastcav>
2020-04-17 19:56:55 -0300

    Configuração do ambiente e adicionada o path de comunicação front/back.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  urlDefault: 'http://localhost:8080/facisaflix-backend/api/v1',
   production: false
 };
 
