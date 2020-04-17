d53e713625539ef65d016ce8e65e94cb548a82c3
lucas tiago <lucastcav>
2020-04-17 20:00:31 -0300

    Criação da recomendação, por enquanto com dados mockados.

--- src/app/modules/private/components/menu-principal/components/menu-principal/menu-principal.component.ts
+++ src/app/modules/private/components/menu-principal/components/menu-principal/menu-principal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaFilmesModel } from 'src/app/modules/private/models/lista-filmes.model';
+import { MenuService } from 'src/app/modules/private/services/menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPrincipalComponent implements OnInit {
   descricaoFilme: string = 'Isolado, intimidado e desconsiderado pela sociedade, o fracassado comediante Arthur Fleck inicia seu caminho como uma mente criminosa após assassinar três homens em pleno metrô. Sua ação inicia um movimento popular contra a elite de Gotham City, da qual Thomas Wayne é seu maior representante.';
+  categoria: string = 'Ação e Suspense';
 
-  constructor() { }
+  clickLike: boolean = false;
+  clickDeslike: boolean = false;
+
+  listaFilmes: Array<ListaFilmesModel>;
+  listaRecomendacoes: Array<ListaFilmesModel>;
+  constructor(private menuService: MenuService) {}
 
   ngOnInit(): void {
+    this.recoveryFilmlist();
+  }
+
+  recoveryFilmlist() {
+    this.menuService.recuperarFilmes().subscribe(lista => {
+      this.listaFilmes = lista;
+    });
+
+  }
+
+
+  likeFilm() {
+    this.clickLike = true;
+    this.clickDeslike = false;
+  }
+
+  deslikeFilm() {
+    this.clickDeslike = true;
+    this.clickLike = false;
+  }
+
+  recoveryAllFilms() {
+
   }
 
 }
