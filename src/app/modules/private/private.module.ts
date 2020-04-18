218bee0f356310fa97604400bbc786425ca5c2b2
lucas tiago <lucastcav>
2020-04-18 04:10:11 -0300

    Import Ngx spinner e form.

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { PrivateRoutingModule } from './private-routing.module';
-import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { FooterComponent } from './components/footer/footer.component';
-import { PainelPrincipalComponent } from './components/painel-principal/painel-principal.component';
+import { HeaderComponent } from './components/header/header.component';
 import { MenuPrincipalComponent } from './components/menu-principal/components/menu-principal/menu-principal.component';
+import { PainelPrincipalComponent } from './components/painel-principal/painel-principal.component';
+import { PrivateRoutingModule } from './private-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, PainelPrincipalComponent, MenuPrincipalComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
     PrivateRoutingModule,
+    NgxSpinnerModule,
   ]
 })
 export class PrivateModule { }
