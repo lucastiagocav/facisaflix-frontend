8755e0226adacb4b7c3e118ffae647934df62609
lucas tiago <lucastcav>
2020-04-18 04:44:29 -0300

    Criação de uma lista mockada. Componente principal preparado para receber carregamento do backend.

---
+++ src/app/modules/private/utils/film-utils.ts
@@ -0,0 +1,49 @@
+
+export class FilmUtils {
+
+  static listaFilme() {
+    return [
+      {
+        id: 1, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 2, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 3, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 4, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 5, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 6, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 7, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+      {
+        id: 8, filme: 'Matrix', categoria: 'Ação e Suspense',
+        descricao: 'Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas Unidades Obrigadas ',
+        urlImg: '../../../../../../../assets/filme-5.png'
+      },
+
+    ];
+  }
+}
